cc119f506c1ab6837807c39c6461f2ae99a49a0d
Paulo <paulo.leite@academico.ifpb.edu.br>
2021-02-23 19:07:17 -0300

    Adicionado cors

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 import './src/database';
 
+import cors from 'cors';
+import helmet from 'helmet';
+
 import express from 'express';
 import { resolve } from 'path';
 
@@ -15,6 +18,20 @@ import tokenRoutes from './src/routes/token';
 import alunoRoutes from './src/routes/aluno';
 import fotoRoutes from './src/routes/foto';
 
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not alowwed by cors'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = express();
@@ -23,6 +40,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
